1d557c5da3934731a545fba4cbabc19b4400542a
Nitya Jain <nityaprofessional6402@gmail.com>
2026-03-13 14:30:00 +0530

    added constants

---
+++ src/lib/utils/constants.ts
@@ -0,0 +1,14 @@
+export const APP_CONFIG = {
+  APP_NAME: "Expense Splitter",
+  VERSION: "1.0.0",
+}
+
+export const SPLIT_TYPES = {
+  EQUAL: "EQUAL",
+  EXACT: "EXACT",
+  PERCENTAGE: "PERCENTAGE",
+}
+
+export const COOKIE_NAMES = {
+  AUTH_TOKEN: "token",
+}
