346724e5d4030bde1bac37bd34342aafe9dc16b4
Nitya Jain <nityaprofessional6402@gmail.com>
2026-03-13 10:15:00 +0530

    added custom errors

---
+++ src/lib/utils/errors.ts
@@ -0,0 +1,29 @@
+export class AppError extends Error {
+  public statusCode: number
+  public isOperational: boolean
+
+  constructor(message: string, statusCode = 500, isOperational = true) {
+    super(message)
+    this.statusCode = statusCode
+    this.isOperational = isOperational
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message = "Resource not found") {
+    super(message, 404)
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message = "Unauthorized access") {
+    super(message, 401)
+  }
+}
+
+export class ValidationError extends AppError {
+  constructor(message = "Invalid input data") {
+    super(message, 400)
+  }
+}
